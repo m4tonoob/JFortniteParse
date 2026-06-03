9d9016f8635a67136b70b7aad0d0e4e69a0f2be4
KingMalivore <rockeef@gmail.com>
2026-06-03 00:38:50 -0400

    ItemDefinitionBase: add isItemNameBlankPlaceholder() for Epic's cooked "Blank" name
    - VIVA BRAZIL

--- src/main/kotlin/me/fungames/jfortniteparse/fort/exports/ItemDefinitionBase.java
+++ src/main/kotlin/me/fungames/jfortniteparse/fort/exports/ItemDefinitionBase.java
@@ -2,10 +2,18 @@ package me.fungames.jfortniteparse.fort.exports;
 
 import me.fungames.jfortniteparse.fort.objects.ItemComponentContainer;
 import me.fungames.jfortniteparse.ue4.objects.core.i18n.FText;
+import me.fungames.jfortniteparse.ue4.objects.core.i18n.FTextHistory;
 
 public class ItemDefinitionBase extends McpItemDefinitionBase {
     public FText ItemName;
     public FText ItemDescription;
     public FText ItemShortDescription;
     public ItemComponentContainer ComponentContainer;
+    
+    public boolean isItemNameBlankPlaceholder() {
+        if (ItemName == null) return false;
+        FTextHistory history = ItemName.getTextHistory();
+        return history instanceof FTextHistory.Base
+            && "Blank".equals(((FTextHistory.Base) history).getSourceString());
+    }
 }
